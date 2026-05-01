4dd64cb22da8ff00797789be76d5216987f14caf
Denni <146395573+dennidc@users.noreply.github.com>
2026-04-30 22:51:50 -0600

    agregando la página del primer proyecto

--- app.js
+++ app.js
@@ -1,52 +1,3 @@
-const triggers = document.querySelectorAll(".lightbox-trigger");
-const btn = document.querySelectorAll(".btnReadMore");
-triggers.forEach((trigger) => {
-  trigger.addEventListener("click", () => {
-    const images = trigger.dataset.images.split(",");
-    let currentIndex = 0;
-
-    // Crear lightbox
-    const lightbox = document.createElement("div");
-    lightbox.classList.add("lightbox");
-
-    lightbox.innerHTML = `
-      <button class="close">&times;</button>
-      <button class="prev">&#10094;</button>
-      <img src="${images[currentIndex]}" alt="">
-      <button class="next">&#10095;</button>
-    `;
-
-    document.body.appendChild(lightbox);
-
-    const img = lightbox.querySelector("img");
-    const next = lightbox.querySelector(".next");
-    const prev = lightbox.querySelector(".prev");
-    const close = lightbox.querySelector(".close");
-
-    // Funciones
-    next.addEventListener("click", () => {
-      currentIndex = (currentIndex + 1) % images.length;
-      img.src = images[currentIndex];
-    });
-
-    prev.addEventListener("click", () => {
-      currentIndex = (currentIndex - 1 + images.length) % images.length;
-      img.src = images[currentIndex];
-    });
-
-    close.addEventListener("click", () => {
-      lightbox.remove();
-    });
-
-    // Cerrar al hacer click fuera
-    lightbox.addEventListener("click", (e) => {
-      if (e.target === lightbox) {
-        lightbox.remove();
-      }
-    });
-  });
-});
-
 /* para que se oculte el menu */
 $("#iconomenu").click(function () {
   $("#contenedor_menu").toggleClass("abrir_menu");
@@ -54,3 +5,29 @@ $("#iconomenu").click(function () {
 $(".cierra").click(function () {
   $("#contenedor_menu").toggleClass("abrir_menu");
 });
+
+const items = document.querySelectorAll(".galeria-item");
+const modal = document.getElementById("modal");
+const imgGrande = document.getElementById("imgGrande");
+const textoModal = document.getElementById("textoModal");
+const cerrar = document.querySelector(".cerrar");
+
+items.forEach(item => {
+  item.addEventListener("click", () => {
+    const img = item.querySelector("img");
+    const titulo = item.querySelector("h3").textContent;
+    const desc = item.querySelector("p").textContent;
+
+    imgGrande.src = img.src;
+    textoModal.innerHTML = `<h3>${titulo}</h3><p>${desc}</p>`;
+    modal.style.display = "block";
+  });
+});
+
+cerrar.onclick = () => modal.style.display = "none";
+
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
